98524772a93786fc09b54a31482ed7fdd0615814
chandu <chandrasekhar0722@gmail.com>
2014-03-09 15:03:27 +0530

    Update views.py
    added new line

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 
 from .models import Institute
-
+#commit
 
 class InstituteCreateView(CreateView):
     model = Institute
